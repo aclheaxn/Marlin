65adb29ddcd0c66fe5011d44a344c06ad8b4c091
Alex Chan <aclheaxn@gmail.com>
2021-09-12 01:29:40 -0400

    new bed defaults

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -642,9 +642,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 174.01
-  #define DEFAULT_bedKi 33.72
-  #define DEFAULT_bedKd 598.60
+  #define DEFAULT_bedKp 218.50
+  #define DEFAULT_bedKi 42.68
+  #define DEFAULT_bedKd 745.81
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
